396e0f27802a2b2fa869d78d39709219af3a2cf2
shy-tan <iabdullahbintahir@gmail.com>
2022-03-20 22:42:05 +0500

    basic layout

--- lib/pages/home/components/home_page_body.dart
+++ lib/pages/home/components/home_page_body.dart
@@ -11,39 +11,59 @@ class HomePageBody extends StatelessWidget {
 
     return Column(
       children: [
-        Container(
-          height: size.height * .15,
-          decoration: const BoxDecoration(
-            color: whiteClr,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(33),
-              bottomRight: Radius.circular(33),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Text(
-                  "Notes",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    color: greyClr,
-                  ),
+        Stack(
+          overflow: Overflow.visible,
+          children: <Widget>[
+            Container(
+              height: size.height * .15,
+              decoration: const BoxDecoration(
+                color: whiteClr,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(33),
+                  bottomRight: Radius.circular(33),
                 ),
-                IconButton(
-                  onPressed: null,
-                  icon: Icon(
-                    Icons.menu,
-                    size: 40,
-                    color: greyClr,
-                  ),
-                )
-              ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Notes",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: greyClr,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: null,
+                      icon: Icon(
+                        Icons.menu,
+                        size: 40,
+                        color: greyClr,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
+            Positioned(
+              // 35 is somehow a magical number here that makes the button
+              // align right were i want
+              // todo test on other phone sizes
+              top: (size.height * 0.15) - 35,
+              left: ((size.width) / 2) - 35,
+              child: const FloatingActionButton(
+                onPressed: null,
+                backgroundColor: darkRedClr,
+                child: Icon(
+                  Icons.add,
+                  size: 30,
+                ),
+              ),
+            )
+          ],
         ),
       ],
     );
