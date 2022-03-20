93b6309ab1e4e9d96bd5ba8cedcebceaec7cbc7e
shy-tan <iabdullahbintahir@gmail.com>
2022-03-20 21:25:08 +0500

    add constants

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const darkRedClr = Color(0xffDC4F64);
+const redClr = Color(0xffCC4F64);
+const darkGreyClr = Color(0xff181A1B);
+const greyClr = Color(0xff212128);
+const whiteClr = Color(0xffffffff);
