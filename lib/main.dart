f4de59c91daa90c2a23e436776f92e9e1e016c5e
shy-tan <iabdullahbintahir@gmail.com>
2022-03-25 18:07:06 +0500

    everything works. "minSdkVersion should be > 21 for using firebase"

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:iproductive/constants.dart';
 import 'package:iproductive/pages/home/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
+
+  void signUp() async {
+    try {
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: 'abdi@gmail.com', password: '123445');
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
 
   // This widget is the root of your application.
   @override
@@ -22,7 +37,15 @@ class MyApp extends StatelessWidget {
           secondary: darkGreyClr,
         ),
       ),
-      home: const HomePage(),
+      // home: const HomePage(),
+      home: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            signUp();
+          },
+          child: const Text("Test the Firebase connection!"),
+        ),
+      ),
     );
   }
 }
